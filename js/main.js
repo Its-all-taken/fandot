1f8d16ce74412ef7e093eb52ad51b7df1c95c651
Maria <53433678+Its-all-taken@users.noreply.github.com>
2021-12-16 21:18:18 +0300

    early resp

--- js/main.js
+++ js/main.js
@@ -3,24 +3,23 @@ $(document).ready(function(){
         let mousePos = { x: -1, y: -1 };
         mousePos.x = event.pageX;
         mousePos.y = event.pageY;
-        if (mousePos.x < 400 && mousePos.y < 400){
+        if (mousePos.x < 300 && mousePos.y < 300 || mousePos.x > 1200 && mousePos.y > 1200){
           $(".main-imag-c").attr("src","images/halo.png");
         }
-        else if (mousePos.x < 500 && mousePos.y < 800){
+        else if (mousePos.x < 600 && mousePos.y < 600){
           $(".main-imag-c").attr("src","images/venti.png");
         }
         else if (mousePos.x < 900 && mousePos.y < 900){
           $(".main-imag-c").attr("src","images/destiny.png");
         }
-        else if (mousePos.x < 1000 && mousePos.y < 400){
+        else if (mousePos.x < 1200 && mousePos.y < 1200){
           $(".main-imag-c").attr("src","images/doctor.png");
         }
       });
     $(".imag-test").click(function(event) {
       $(this).toggleClass("imag-test-op");
       let testSelect = $('.imag-test-op').length;
-      console.log('testSelect');
       $('.result').empty();
-      $('.result').append(testSelect * 12.5+'%');
+      $('.result').append(testSelect * 12.5 + '%');
     });
   });
