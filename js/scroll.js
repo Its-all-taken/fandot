db8c130ecc28efe34264985fdb22fe38b6dde332
Maria <53433678+Its-all-taken@users.noreply.github.com>
2021-12-21 14:20:42 +0300

    smooth scroll

---
+++ js/scroll.js
@@ -0,0 +1,12 @@
+$(function() {
+$('.menu-a').each(function() {
+$(this).on('click', function(e) {
+  e.preventDefault();
+  let scrollTo = $(this).attr('href');
+  let whitespace = scrollTo == '#s1' ? 70 : 30;
+  $('html,body').animate({
+    scrollTop: $(scrollTo).offset().top - whitespace
+  }, 1000);
+});
+});
+});
